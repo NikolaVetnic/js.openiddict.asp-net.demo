1ff3db4e81b7825ff8de6e438964466230cedfd0
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2023-08-17 13:45:14 +0200

    chapter 3 done

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace AuthorizationServer;
 
@@ -13,6 +18,51 @@ public class Startup
             {
                 options.LoginPath = "/account/login";
             });
+
+        services.AddDbContext<DbContext>(options =>
+        {
+            // Configure the context to use an in-memory store.
+            options.UseInMemoryDatabase(nameof(DbContext));
+
+            // Register the entity sets needed by OpenIddict.
+            options.UseOpenIddict();
+        });
+
+        services.AddOpenIddict()
+
+        // Register the OpenIddict core components.
+        .AddCore(options =>
+        {
+            // Configure OpenIddict to use the EF Core stores/models.
+            options.UseEntityFrameworkCore()
+                .UseDbContext<DbContext>();
+        })
+
+        // Register the OpenIddict server components.
+        .AddServer(options =>
+        {
+            options
+                .AllowClientCredentialsFlow();
+
+            options
+                .SetTokenEndpointUris("/connect/token");
+
+            // Encryption and signing of tokens
+            options
+                .AddEphemeralEncryptionKey()
+                .AddEphemeralSigningKey()
+                .DisableAccessTokenEncryption();
+
+            // Register scopes (permissions)
+            options.RegisterScopes("api");
+
+            // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+            options
+                .UseAspNetCore()
+                .EnableTokenEndpointPassthrough();
+        });
+
+        services.AddHostedService<TestData>();
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
